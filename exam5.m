5223e5e4871b2ec0022f847710d59dd966eb5759
nan mu <mu.nan.11@outlook.com>
2024-10-06 22:07:58 +0800

    格式化代码

--- exam5.m
+++ exam5.m
@@ -1,13 +1,18 @@
 syms n;
 x = (0.8) ^ n;
 h = (-0.9) ^ n;
+% 第一小问
 y1 = conv(double(subs(x, n, 0:25)), double(subs(h, n, 0:25)));
-stem(-25:25, y1);
-hold on;
+% 第二小问
 y2 = filter(double(subs(h, n, 0:25)), 1, double(subs(x, n, 0:25)));
+% 对齐y1
 y2_padded = [y2, zeros(1, length(-25:25) - length(y2))];
+
+hold on;
+stem(-25:25, y1);
 stem(-25:25, y2_padded, 'r^');
 hold off;
+
 grid on;
 title('卷积结果');
 legend('conv', 'filter');
