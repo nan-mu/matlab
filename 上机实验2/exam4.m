2ec751fb2e5e6b807be2ce7b9aea6117cd70d1da
nan mu <mu.nan.11@outlook.com>
2024-10-28 18:57:00 +0800

    完成第四第五

--- 上机实验2/exam4.m
+++ 上机实验2/exam4.m
@@ -7,14 +7,25 @@ A(A == -12345.0) = NaN;
 B(B == -12345) = NaN;
 fclose(fid);
 
-% Compute the FFT of HR
-HR_fft = fft(HR, 13);
+% 采样频率
+fs = 20; % Hz
 
-% Create a new figure
+% 计算 FFT
+HR_fft = fft(HR);
+
+% 计算频率轴
+N = length(HR);
+f = (0:N - 1) * (fs / N);
+
+% 选择 0.01 Hz 到 10 Hz 范围内的频率分量
+freq_range = (f >= 0.01) & (f <= 10);
+f_selected = f(freq_range);
+HR_fft_selected = HR_fft(freq_range);
+
+% 绘制频谱
 figure;
-
-% Plot the magnitude
-plot(abs(HR_fft));
-title('Magnitude of HR FFT');
-xlabel('Frequency (Hz)');
-ylabel('Magnitude');
+plot(f_selected, abs(HR_fft_selected));
+title('信号的频谱');
+xlabel('频率 (Hz)');
+ylabel('幅度');
+grid on;
