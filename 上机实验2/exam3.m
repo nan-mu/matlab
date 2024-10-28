449dd3340c877393adfb04cbc6cb48934b7baeeb
nan mu <mu.nan.11@outlook.com>
2024-10-28 19:39:44 +0800

    修改为子图

--- 上机实验2/exam3.m
+++ 上机实验2/exam3.m
@@ -12,14 +12,32 @@ X2 = fft(x2, 101);
 x3 = double(subs(x, n, 0:499));
 X3 = fft(x3, 500);
 
-% Plotting
+% 绘制在竖着的三张子图上
 figure;
-hold on;
 
-stem(linspace(0, 500, length(X1)), abs(X1), 'DisplayName', 'X1-11点fft');
-plot(linspace(0, 500, length(X2)), abs(X2), 'DisplayName', 'X2-零填充101点fft');
-plot(linspace(0, 500, length(X3)), abs(X3), 'DisplayName', 'X3-500点fft');
+% 绘制 X1 的频谱
+subplot(3, 1, 1);
+stem(0:length(X1) - 1, abs(X1), 'filled');
+title('X1 - 11点 FFT');
+xlabel('频率 (Hz)');
+ylabel('幅度');
+grid on;
 
+% 绘制 X2 的频谱
+subplot(3, 1, 2);
+plot(0:length(X2) - 1, abs(X2));
+title('X2 - 零填充 101点 FFT');
+xlabel('频率 (Hz)');
+ylabel('幅度');
+grid on;
+
+% 绘制 X3 的频谱
+subplot(3, 1, 3);
+plot(0:length(X3) - 1, abs(X3));
+title('X3 - 500点 FFT');
+xlabel('频率 (Hz)');
+ylabel('幅度');
+grid on;
 set(gca, 'XTick', []);
 set(gca, 'YTick', []);
 legend;
